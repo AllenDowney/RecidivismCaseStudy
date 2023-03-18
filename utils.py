2ab84e87c445725829a64f69e18a6cebc2bbde98
Allen Downey <downey@allendowney.com>
2023-03-18 15:06:32 -0400

    Transposing the confusion matrix

--- utils.py
+++ utils.py
@@ -29,35 +29,29 @@ def make_matrix(cp, threshold=4):
 
     returns: DataFrame containing the confusion matrix
     """
-    # recode the decile scores
-    a = np.where(cp['decile_score'] > threshold,
-                 'Positive',
-                 'Negative')
-    high_risk = pd.Series(a, name='Predicted')
+    a = np.where(cp["decile_score"] > threshold, "Pred Positive", "Pred Negative")
+    high_risk = pd.Series(a, name="")
 
-    # recode the recidivism outcomes
-    a = np.where(cp['two_year_recid'] == 1,
-                 'Condition',
-                 'No Condition')
-    new_charge_2 = pd.Series(a, name='Actual')
+    a = np.where(cp["two_year_recid"] == 1, "Positive", "Negative")
+    new_charge = pd.Series(a, name="Actual")
 
-    # cross tabulate
-    matrix = pd.crosstab(high_risk, new_charge_2)
+    matrix = pd.crosstab(new_charge, high_risk)
+    matrix.sort_index(axis=0, ascending=False, inplace=True)
+    matrix.sort_index(axis=1, ascending=False, inplace=True)
 
     # make sure all four elements are present
     index = ['Positive', 'Negative']
-    columns = ['Condition', 'No Condition']
+    columns = ['Pred Positive', 'Pred Negative']
     matrix = matrix.reindex(index=index, columns=columns, fill_value=0)
-
+    
     return matrix
 
 
 def percent(x, y):
-    """Compute the percentage `x/(x+y)*100`.
-    """
-    if x+y == 0:
+    """Compute the percentage `x/(x+y)*100`."""
+    if x + y == 0:
         return np.nan
-    return x / (x+y) * 100
+    return x / (x + y) * 100
 
 
 def predictive_value(m):
@@ -65,7 +59,7 @@ def predictive_value(m):
 
     m: confusion matrix
     """
-    tp, fp, fn, tn = m.to_numpy().flatten()
+    tp, fn, fp, tn = m.to_numpy().flatten()
     ppv = percent(tp, fp)
     npv = percent(tn, fn)
     return ppv, npv
@@ -76,7 +70,7 @@ def sens_spec(m):
 
     m: confusion matrix
     """
-    tp, fp, fn, tn = m.to_numpy().flatten()
+    tp, fn, fp, tn = m.to_numpy().flatten()
     sens = percent(tp, fn)
     spec = percent(tn, fp)
     return sens, spec
@@ -87,23 +81,23 @@ def error_rates(m):
 
     m: confusion matrix
     """
-    tp, fp, fn, tn = m.to_numpy().flatten()
+    tp, fn, fp, tn = m.to_numpy().flatten()
     fpr = percent(fp, tn)
     fnr = percent(fn, tp)
     return fpr, fnr
 
 
-def prevalence(df):
+def prevalence(m):
     """Compute prevalence.
 
     m: confusion matrix
     """
-    tp, fp, fn, tn = df.to_numpy().flatten()
-    prevalence = percent(tp+fn, tn+fp)
+    tp, fn, fp, tn = m.to_numpy().flatten()
+    prevalence = percent(tp + fn, tn + fp)
     return prevalence
 
 
-def compute_metrics(m, name=''):
+def compute_metrics(m, name=""):
     """Compute all metrics.
 
     m: confusion matrix
@@ -114,11 +108,11 @@ def compute_metrics(m, name=''):
     ppv, npv = predictive_value(m)
     prev = prevalence(m)
 
-    index = ['FPR', 'FNR', 'PPV', 'NPV', 'Prevalence']
-    df = pd.DataFrame(index=index, columns=['Percent'])
+    index = ["FPR", "FNR", "PPV", "NPV", "Prevalence"]
+    df = pd.DataFrame(index=index, columns=["Percent"])
     df.Percent = fpr, fnr, ppv, npv, prev
     df.index.name = name
-    return df
+    return df.round(1)
 
 
 def calibration_curve(df):
@@ -128,29 +122,30 @@ def calibration_curve(df):
 
     returns: Series
     """
-    grouped = df.groupby('decile_score')
-    return grouped['two_year_recid'].mean()
+    grouped = df.groupby("decile_score")
+    return grouped["two_year_recid"].mean()
 
 
 def decorate(**options):
     """Decorate the current axes.
-    
+
     Call decorate with keyword arguments like
     decorate(title='Title',
              xlabel='x',
              ylabel='y')
-             
+
     The keyword arguments can be any of the axis properties
     https://matplotlib.org/api/axes_api.html
     """
     ax = plt.gca()
     ax.set(**options)
-    
+
     handles, labels = ax.get_legend_handles_labels()
     if handles:
         ax.legend(handles, labels)
 
     plt.tight_layout()
+
 
 def constant_predictive_value(ppv, npv, prev):
     """Make a confusion matrix with given metrics.
@@ -164,10 +159,14 @@ def constant_predictive_value(ppv, npv, prev):
     ppv /= 100
     npv /= 100
     prev /= 100
-    a = np.array((ppv*(npv + prev - 1)/(npv + ppv - 1),
-                -(ppv - 1)*(npv + prev - 1)/(npv + ppv - 1),
-                -(npv - 1)*(ppv - prev)/(npv + ppv - 1),
-                npv*(ppv - prev)/(npv + ppv - 1)))
+    a = np.array(
+        (
+            ppv * (npv + prev - 1) / (npv + ppv - 1),
+            -(ppv - 1) * (npv + prev - 1) / (npv + ppv - 1),
+            -(npv - 1) * (ppv - prev) / (npv + ppv - 1),
+            npv * (ppv - prev) / (npv + ppv - 1),
+        )
+    )
 
     return pd.DataFrame(a.reshape((2, 2)))
 
@@ -185,7 +184,7 @@ def run_cpv_model(cp):
     ppv, npv = predictive_value(matrix_all)
     prevalences = np.linspace(35, 55, 11)
 
-    pred_er = pd.DataFrame(columns=['fpr', 'fnr'])
+    pred_er = pd.DataFrame(columns=["fpr", "fnr"])
 
     for prev in prevalences:
         m = constant_predictive_value(ppv, npv, prev)
@@ -199,10 +198,13 @@ def plot_cpv_model(pred_er):
 
     pred_er: DataFrame of predicted error rates
     """
-    pred_er['fpr'].plot(label='Predicted FPR', color='C2')
-    pred_er['fnr'].plot(label='Predicted FNR', color='C4')
-    decorate(xlabel='Prevalence', ylabel='Percent',
-             title='Error rates, constant predictive value')
+    pred_er["fpr"].plot(label="Predicted FPR", color="C2")
+    pred_er["fnr"].plot(label="Predicted FNR", color="C4")
+    decorate(
+        xlabel="Prevalence",
+        ylabel="Percent",
+        title="Error rates, constant predictive value",
+    )
 
 
 def constant_error_rates(fpr, fnr, prev):
@@ -217,8 +219,7 @@ def constant_error_rates(fpr, fnr, prev):
     prev /= 100
     fpr /= 100
     fnr /= 100
-    a = [[prev*(1 - fnr),  fpr*(1 - prev)],
-         [fnr*prev,       (fpr - 1)*(prev - 1)]]
+    a = [[prev * (1 - fnr), fpr * (1 - prev)], [fnr * prev, (fpr - 1) * (prev - 1)]]
 
     return pd.DataFrame(a)
 
@@ -236,7 +237,7 @@ def run_cer_model(cp):
     fpr, fnr = error_rates(matrix_all)
     prevalences = np.linspace(35, 65, 11)
 
-    pred_pv = pd.DataFrame(columns=['ppv', 'npv'])
+    pred_pv = pd.DataFrame(columns=["ppv", "npv"])
 
     for prev in prevalences:
         m = constant_error_rates(fpr, fnr, prev)
@@ -250,13 +251,17 @@ def plot_cer_model(pred_pv):
 
     pred_er: DataFrame of predicted error rates
     """
-    pred_pv['ppv'].plot(label='Predicted PPV', color='C0')
-    pred_pv['npv'].plot(label='Predicted NPV', color='C1')
-    decorate(xlabel='Prevalence', ylabel='Percent',
-             title='Predictive value, constant error rates')
+    pred_pv["ppv"].plot(label="Predicted PPV", color="C0")
+    pred_pv["npv"].plot(label="Predicted NPV", color="C1")
+    decorate(
+        xlabel="Prevalence",
+        ylabel="Percent",
+        title="Predictive value, constant error rates",
+    )
 
 
 from scipy.interpolate import interp1d
+
 
 def interpolate(series, value, **options):
     """Evaluate a function at a value.
@@ -293,13 +298,13 @@ def sweep_threshold(cp):
              one column for each metric
     """
     index = range(0, 11)
-    columns = ['FPR', 'FNR', 'PPV', 'NPV', 'Prevalence']
+    columns = ["FPR", "FNR", "PPV", "NPV", "Prevalence"]
     table = pd.DataFrame(index=index, columns=columns, dtype=float)
 
     for threshold in index:
         m = make_matrix(cp, threshold)
         metrics = compute_metrics(m)
-        table.loc[threshold] = metrics['Percent']
+        table.loc[threshold] = metrics["Percent"]
 
     return table
 
@@ -311,24 +316,22 @@ def plot_roc(table, **options):
            classification threshold
     options: passed to plot
     """
-    plt.plot([0,100], [0,100], ':', color='gray')
-    sens = 100-table['FNR']
-    plt.plot(table['FPR'], sens, **options)
-    decorate(xlabel='FPR',
-             ylabel='Sensitivity (1-FNR)',
-             title='ROC curve')
+    plt.plot([0, 100], [0, 100], ":", color="gray")
+    sens = 100 - table["FNR"]
+    plt.plot(table["FPR"], sens, **options)
+    decorate(xlabel="FPR", ylabel="Sensitivity (1-FNR)", title="ROC curve")
 
 
 from scipy.integrate import simps
+
 
 def compute_auc(table):
     """Compute the area under the ROC curve.
 
     Uses the trapezoid rule, so
     """
-    y = 100-table['FNR']
-    x = table['FPR']
+    y = 100 - table["FNR"]
+    x = table["FPR"]
     y = y.sort_index(ascending=False) / 100
     x = x.sort_index(ascending=False) / 100
     return simps(y.values, x.values)
-    
